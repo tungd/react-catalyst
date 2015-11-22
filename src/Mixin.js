d0e55f7a267c393196bcee586f561588993c9f90
Tung Dao <me@tungdao.com>
2015-11-22 16:10:30 +0700

    simple mixin from object

--- src/Mixin.js
+++ src/Mixin.js
@@ -1,6 +1,23 @@
 
-var Mixin = {
+let Mixin = (type) => {
+  type.with = Mixin.with.bind(null, type);
+  return type;
+};
 
+Mixin.with = (base, ...mixins) => {
+  mixins.forEach((mixin) => {
+    // console.log(typeof mixin);
+    // console.log(Object.getOwnPropertyNames(mixin));
+
+    if (typeof mixin === 'object') {
+      Object.getOwnPropertyNames(mixin).forEach((method) => {
+        base.prototype[method] = mixin[method];
+      });
+    }
+
+  });
+
+  return base;
 };
 
 export default Mixin;
--- test/MixinTest.js
+++ test/MixinTest.js
@@ -1,15 +1,49 @@
-/*global jest describe it expect*/
+/*global describe it*/
 
-var __ = require('assert');
+var _ = require('assert');
 
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import Mixin from '../src/Mixin';
 
 describe('Mixin', () => {
 
-  it('should be defined', () => {
-    __.notEqual(Mixin, null);
+  it('is a function', () => {
+    _.notEqual(Mixin, undefined);
+    _.equal(typeof Mixin, 'function');
   });
 
-  // describe('#with')
+  describe('#with', () => {
 
+    it('is exposed', () => {
+      _.notEqual(Mixin.with, undefined);
+      _.equal(typeof Mixin.with, 'function');
+    });
+
+    describe('assigns the method to new component', () => {
+      class TestMixinComponent extends React.Component {
+        render() {
+          return <span>{this.message()}</span>;
+        }
+      }
+
+      it('from object mixin', () => {
+        var TestMixinMethod = {
+          message: function() {
+            return "MESSAGE";
+          }
+        };
+
+        var element = React.createElement(
+          Mixin(TestMixinComponent).with(TestMixinMethod),
+          null, null);
+
+        _.ok(/MESSAGE/.test(ReactDOMServer.renderToString(element)));
+      });
+
+      it('from class mixin', () => {
+
+      });
+    });
+  });
 });
